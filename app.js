a22d3afa822a5981fa2a691920ca1bfec9e50f5a
Ljiljana J <ljiljajanjic@gmail.com>
2023-08-22 15:00:44 -0400

    Week 12: A Front End for the Jobs API

--- app.js
+++ app.js
@@ -31,9 +31,10 @@ app.use(cors())
 app.use(xss())
 
 
-app.get('/', (req, res) => {
-  res.send('jobs api')
-})
+//app.get('/', (req, res) => {
+  //res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+//})
+app.use(express.static('public'))
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
